e5ea47b4ad1480623ae543f84c76c065d251785f
Ohyeon <rest515@gmail.com>
2022-11-22 20:39:39 +0900

    감축수단을 목록으로 보여줍니다

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @root_goals = Goal.roots
+    @tools = Tool.all
     @data_sources = DataSource.all
     @stories = Story.all
     @policies = Policy.all
