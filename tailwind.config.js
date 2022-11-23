0c5132d17457dff889f701199087ccc3e5c8851e
Ohyeon <rest515@gmail.com>
2022-11-23 16:24:49 +0900

    컨테이너 폭과 패딩을 지정합니다

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,16 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  theme: {
+    container: {
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '1024px',
+        'xl': '1024px',
+        '2xl': '1024px',
+      },
+      padding: '1rem',
+    }
+  }
 }
